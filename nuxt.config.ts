3071fe73c958b38c569d21b0b69eeacd7fef3d7c
Oke Michael <31992802+mykelcodex@users.noreply.github.com>
2026-06-05 02:50:00 +0100

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       laravelUrl:
-        process.env.NUXT_PUBLIC_LARAVEL_URL || "http://localhost:8080",
+        process.env.NUXT_PUBLIC_LARAVEL_URL || "http://localhost:8007",
       emitwaveApiUrl:
         process.env.NUXT_PUBLIC_EMITWAVE_API_URL || "http://localhost:8080",
       emitwaveRealtimeUrl:
